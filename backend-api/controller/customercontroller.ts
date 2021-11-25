c9a4ea5b9825123935c1ef493ae245d24b964522
vedashri07 <vedashrimungekar07@gmail.com>
2021-11-25 12:52:38 +0530

    validation, bcryptjs, compare paswords ,jwt token for login

--- backend-api/controller/customercontroller.ts
+++ backend-api/controller/customercontroller.ts
@@ -1,77 +1,85 @@
 const CustomerModel = require('../model/customermodel.ts');
-const sha256 = require('sha256');
 const jwttoken = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
 
 exports.createCustomer = (req, res, next) => {
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
+    var hashedPassword;
+    const name = req.body.name;
+    const email_id = req.body.email_id;
+    const mobile_no = req.body.mobile_no;
+    const password = req.body.password;
+    bcrypt.genSalt(10, function (err, Salt) {
 
-    else {
-        const name = req.body.name;
-        const email_id = req.body.email_id;
-        const mobile_no = req.body.mobile_no;
-        const password = sha256(req.body.password);
+        //encrypting password
+        bcrypt.hash(password, Salt, function (err, hash) {
 
-        const Customer = new CustomerModel(
-            {
-                name: name,
-                email_id: email_id,
-                mobile_no: mobile_no,
-                password: password,
-                createdBy: 'customer',
-                usertype: 'customer',
-                createdOn: new Date()
-            });
-
-        CustomerModel.find({ name: name, email_id: email_id, isDeleted: false }, function (err, user) {
-            if (user.length > 0) {
-
-                res.json({ message: "User already exists " })
+            if (err) {
+                return console.log('CANNOT ENCRYPT');
             }
-            else {
-                Customer
-                    .save().then(result => {
-                        console.log(result);
+            hashedPassword = hash;
 
-                        var token = jwttoken.sign({ name: name }, "" + process.env.TOKEN_SECRET, { expiresIn: 60 * 60 })
-
-
-                        res.status(200).send({ auth: true, token: token });
-
-
-                    })
-                    .catch(err => {
-                        console.log(err.message);
-                    })
-            }
+        })
+    })
+    const Customer = new CustomerModel(
+        {
+            name: name,
+            email_id: email_id,
+            mobile_no: mobile_no,
+            password: hashedPassword,
+            createdBy: 'customer',
+            usertype: 'customer',
+            createdOn: new Date()
         });
 
+    CustomerModel.find({ email_id: email_id, isDeleted: false }, function (err, user) {
+        if (user.length > 0) {
 
-    }
+            res.json({ message: "User already exists " })
+        }
+        else {
+            Customer
+                .save().then(result => {
+                    console.log(result);
+
+                    var token = jwttoken.sign({ id: user._id }, "" + process.env.TOKEN_SECRET, { expiresIn: 60 * 60 })
+
+
+                    res.status(200).send({ auth: true, token: token });
+
+
+                })
+                .catch(err => {
+                    console.log(err.message);
+                })
+        }
+    });
+
 
 }
 
 
 
+
+
 //for login
 exports.postLogin = (req, res, next) => {
-    console.log(req.body);
-    const name = req.body.name;
-    const password = sha256(req.body.password);
-    CustomerModel.find({ name: name, password: password, isDeleted: false }, function (err, user) {
-        if (user.length > 0) {
 
-            res.json({ message: "Login successfully", status: true })
-        }
-        else {
-            res.json("user not found")
-        }
-    });
+    var hashedPassword;
+    const email_id = req.body.email_id;
+    CustomerModel.find({ email_id: email_id, isDeleted: false }, function (err, user) {
+        bcrypt.compare(req.body.password, 'superSecret', function (err, res) {
+            if (req.body.password != CustomerModel.password) {
+                res.json({ success: false, message: 'passwords do not match' });
+            } else {
+                var token = jwttoken.sign({ id: user._id }, "" + process.env.TOKEN_SECRET, { expiresIn: 60 * 60 })
+
+
+                res.status(200).send({ message: "Login Succesful", auth: true, token: token });
+            }
+        });
+    })
 }
 
 //for update
--- backend-api/validation/validator.ts
+++ backend-api/validation/validator.ts
@@ -5,7 +5,16 @@ exports.createUser = [
     check('email_id').isEmail(),
     check('email_id').notEmpty(),
     check('name').notEmpty(),
-    check('password').notEmpty(),
+    check('password', 'Password field is required').not().isEmpty(),
+    check('password').isLength({ min: 3 }).withMessage('Password must be at least 3 chars long'),
+    check('password').matches(/\d/).withMessage('Password must contain a number'),
+    check('password2').custom((value, {req}) => { 
+      //custom validator
+      if(value!==req.body.password) {
+        throw new Error('Passwords doesn\'t match');
+      }
+      return true;
+    }),
     check('mobile_no').notEmpty(),
     check('mobile_no').isNumeric(),
 
